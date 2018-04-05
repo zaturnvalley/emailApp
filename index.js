6972f3298b9d658efc65377bc4ef7fdbfd996691
zaturnvalley <zacharycmason@gmail.com>
2018-04-04 20:36:30 -0700

    pulled in GoogleStrat routes

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+passport.use(
+    new GoogleStrategy({
+      clientID: 'string',
+      clientSecret: 'string',
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT)
