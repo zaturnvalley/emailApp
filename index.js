4044c2791938e5239f9d7c78a6b5fa4b39f3d239
zaturnvalley <zacharycmason@gmail.com>
2018-04-04 20:32:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT)
